2858fdd105c07ff7c8a070b40f94e1d57a97434e
evelyntys <evelyn.tan@trentglobal.edu.sg>
2022-08-06 16:45:45 +0000

    initial commit with base express template

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const hbs = require('hbs');
+const wax = require('wax-on');
+require('dotenv').config();
+
+let app = express();
+
+app.set('view engine', 'hbs');
+
+app.use(express.static('public'));
+
+wax.on(hbs.handlebars);
+wax.setLayoutPath('./views/layouts');
+
+app.use(express.urlencoded({
+    extended: false
+}))
+
+app.get('/', function(req,res){
+    // res.send('welcome to the api for figuya')
+    res.render('index')
+})
+
+app.listen(3000, function(){
+    console.log('server started')
+})
